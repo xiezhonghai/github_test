cbd59f3a23be8ebe8271b68300d7ed463e0c4ceb
xiezhonghai <934605926@qq.com>
2019-07-13 14:43:29 +0800

    test

---
+++ springBoot-example/springBoot-base/src/main/java/com/backstage/base/config/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.backstage.base.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.stereotype.Component;
+
+/**
+ * redis序列化类
+ */
+@Component
+public class RedisConfig {
+
+    @SuppressWarnings("rawtypes")
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired(required = false)
+    public void setRedisTemplate(RedisTemplate redisTemplate) {
+        RedisSerializer stringSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringSerializer);
+        redisTemplate.setValueSerializer(stringSerializer);
+        redisTemplate.setHashKeySerializer(stringSerializer);
+        redisTemplate.setHashValueSerializer(stringSerializer);
+        this.redisTemplate = redisTemplate;
+    }
+
+}
